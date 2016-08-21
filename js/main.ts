00335f5703d80bcd56c73a13ce236b88281f7874
miclairy <clb.miclairy@gmail.com>
2016-08-21 12:00:10 +1200

    application insights

--- js/main.ts
+++ js/main.ts
@@ -1,23 +1,26 @@
-function toggleStars() {
-    if ($(".star").hasClass("glyphicon glyphicon-star-empty star")) {
-        console.log("empty");
+function toggleStars(): void{
+    if ($(".star").hasClass("glyphicon glyphicon-star-empty star")){ 
+        console.log("empty")
         $(".star").addClass("glyphicon-star").removeClass("glyphicon-star-empty");
-    }
-    else {
-        console.log("black");
+    } else {
+        console.log("black")
         $(".star").addClass("glyphicon-star-empty").removeClass("glyphicon-star");
     }
-}
-;
-$(document).ready(function () {
-    $("#myCarousel").carousel({ interval: 50000 });
+};
+
+$(document).ready(function() {
+    $("#myCarousel").carousel({interval: 50000});
     $("#hidden").hide();
-    $("#imageHid").on("mouseenter", function () {
+
+
+    $("#imageHid").on("mouseenter", function(){
         $("#hidden").fadeIn();
         console.log("entered mouse on image");
     });
-    $("#imageHid").on("mouseleave", function () {
+
+    $("#imageHid").on("mouseleave", function(){
         $("#hidden").fadeOut();
         console.log("exit mouse on image");
     });
+
 });
