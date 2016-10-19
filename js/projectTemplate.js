146c3d7326f104a6fe287f098d50bdb085babeaf
claire <cba62@uclive.ac.nz>
2016-10-19 22:03:52 +1300

    when click on photo get relevent large photo and infor page

--- js/projectTemplate.js
+++ js/projectTemplate.js
@@ -4,27 +4,129 @@ $(document).ready(function () {
     var project = "";
     var src = "";
     var alt = "";
+    var title = "";
+
+    var duckProj = {
+        src : "images/Duck.png",
+        alt : "Rubber ducks in a bath tub",
+        title : "Duck!",
+        youtube : "#",
+        description : "The reddit monthly contest for July was Duck! themed. Thus of course I made ducks falling from the sky",
+    };
+
+    var owlProj = {
+        src : "images/owl.png",
+        alt : "Animated owl sitting in an hole in a tree",
+        title : "Early Bird",
+        youtube : "#",
+        description : "I made an animation of an owl falling his hole.",
+    };
+
+    var donutProj = {
+        src : "images/donutCup.png",
+        alt : "Donut and tea cup",
+        title : "Donut and Tea",
+        youtube : "#",
+        description : "I tried to make a photo realistic photo of a cup and donut",
+    };
+    
+    var cokeProj = {
+        src : "images/cocacolaBottle.png",
+        alt : "Coke bottle falling from the sky",
+        title : "Cocacola bottle",
+        youtube : "#",
+        description : "Reddit's theme contest for June was to texture a bottle and I made a coke bottle falling out of the sky",
+    };
+    
+    var pigletProj = {
+        src : "images/piglet.png",
+        alt : "A still from the animation piglet and the red balloon",
+        title : "Piglet and the Red Balloon",
+        youtube : "#",
+        description : "My first animation without a tutorial. Piglet loses his balloon then has to climb a tree to get it.",
+    };
+    
+    var lowPolyProj = {
+        src : "images/low poly owl and tree.png",
+        alt : "Low poly owl and tree on floating island",
+        title : "Low poly floating island",
+        youtube : "#",
+        description : "A low poly tree and owl",
+    };
+    
+    var mechHeartProj = {
+        src : "images/mechanicalHeart.png",
+        alt : "Mechanical Heart",
+        title : "Mechanical Heart",
+        youtube : "#",
+        description : "A science fiction version of what a mechanical heart could look like.",
+    };
+    
+    var motorbikeProj = {
+        src : "images/motorbike.png",
+        alt : "motorbike",
+        title : "Motorbike",
+        youtube : "#",
+        description : "Harely Davidson Motorbike",
+    };
+
+    var gunProj = {
+        src : "images/gun.png",
+        alt : "gun",
+        title : "Cyber punk Gun",
+        youtube : "#",
+        description : "A cyberpunk gun that I made",
+    };
+    
+    var bearProj = {
+        src : "images/old bear.png",
+        alt : "old bear",
+        title : "Old Bear",
+        youtube : "#",
+        description : "An old teddy bear inspired from the childrens book old bear",
+    };
+
+    var perioProj = {
+        src : "images/perio.png",
+        alt : "perio",
+        title : "Perio",
+        youtube : "#",
+        description : "An animation of a bird waking up in the morning based on the tutorial",
+    };
+
+    var projects = [duckProj, donutProj, owlProj, cokeProj, pigletProj,
+        lowPolyProj, mechHeartProj, motorbikeProj, gunProj, bearProj, perioProj];
 
     $('.btn').click( function(){
         localStorage.project = $(this).attr("title");
-        localStorage.src = $(this).attr("src");
-        localStorage.alt = $(this).attr("alt");
-        console.log(src);    
+        console.log(localStorage.project);
         document.location.href = "project.html";
     });
-    var data = {
-        imageSrc: localStorage.src,
-        altImage:  localStorage.alt,
-        youtube: "#",
-        description: "to do laters"
-    }
+
+
+
+    // var data = {
+    //     imageSrc: localStorage.src,
+    //     altImage:  localStorage.alt,
+    //     youtube: "#",
+    //     description: "to do laters"
+    // }
     
     console.log("moved to " + localStorage.project);
 
 
     var template = $("#template").html();
     console.log(template);
-    var html = Mustache.render(template, data);
+    
+
+    for (i = 0; i < projects.length; i++){
+        
+        if (projects[i].title == localStorage.project){
+            console.log(projects[i]);
+            var html = Mustache.render(template, projects[i]);
+        }
+    }
+
     $("body").append(html);
 
 
